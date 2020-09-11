1b0618caf18292f26c864008630d1afa8af43a1a
Hadley <Hadley_hzy@hotmail.com>
2020-09-11 15:08:24 +0200

    update

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from __future__ import print_function
 import torch
-x = torch.rand(5, 3)
+
+x = torch.ones(2,2,requires_grad=True)
+print(x)
+
+y = x + 2
+print(y)
+
+#scalar cannot show calculated backward unless using tensor as additional input
+i = torch.ones(2,2)
+y.backward(i)
+print(x.grad)
 print(x)
